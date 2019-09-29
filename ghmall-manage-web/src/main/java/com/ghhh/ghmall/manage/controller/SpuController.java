18e93100a3b68776754e850563a326ba56f7ccb2
galaxyll <2533140373@qq.com>
2019-09-29 17:03:54 +0800

    接收商品图片但无处理

--- ghmall-manage-web/src/main/java/com/ghhh/ghmall/manage/controller/SpuController.java
+++ ghmall-manage-web/src/main/java/com/ghhh/ghmall/manage/controller/SpuController.java
@@ -1,11 +1,11 @@
 package com.ghhh.ghmall.manage.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.ghhh.ghmall.bean.PmsBaseSaleAttr;
 import com.ghhh.ghmall.bean.PmsProductInfo;
 import com.ghhh.ghmall.service.SpuService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -32,6 +32,13 @@ public class SpuController {
     public void saveSpuInfo(@RequestBody PmsProductInfo pmsProductInfo)
     {
         System.out.println(pmsProductInfo);
+    }
+
+    @RequestMapping("/fileUpload")
+    @ResponseBody
+    public String saveMedia(@RequestParam("file") MultipartFile multipartFile)
+    {
+        return "https://img30.360buyimg.com/sku/jfs/t27322/82/1288462069/147660/2d0ce757/5bc57779Na5ca3268.jpg";
     }
 
 }
